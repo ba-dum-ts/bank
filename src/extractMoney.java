1845063f1ba931777347ebe673645d8388764d2b
ba-dum-ts <carlos.leyton@elev.cybergymnasiet.se>
2020-04-03 14:04:43 +0200

    mimameds mamma

--- src/extractMoney.java
+++ src/extractMoney.java
@@ -21,25 +21,24 @@ public class extractMoney extends JPanel{
 
     public void menu(){ // hur mycket användaren väljer att ta bort
         label.setBounds(210, 160, 500, 23);
-        label.setText("What amount do you wish to extract from your account:");
         label.setFont(new Font("TimesRoman", Font.PLAIN, 20));
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setText("What amount do you wish to extract from your account:");
 
         incomeField.setBounds(350, 200, 200, 23);
-        incomeField.setText(Double.toString(afterLogin.income));
         incomeField.setHorizontalAlignment(SwingConstants.CENTER);
+        incomeField.setText(Double.toString(afterLogin.income));
+
+        incomeField.addMouseListener(new MouseAdapter() { // tömmer ruta
+            public void mouseClicked(MouseEvent e) {
+                incomeField.setText("");
+            }
+        });
 
         incomeField.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                loop = true;
-                while(loop){
-                    incomeField.addMouseListener(new MouseAdapter() { // tömmer ruta
-                        public void mouseClicked(MouseEvent e) {
-                            incomeField.setText("");
-                        }
-                    });
-
+                try{
                     if(Double.parseDouble(incomeField.getText()) >= 0){ // om antalet användaren skriver är mer eller lika med noll
                         if(afterLogin.income < Double.parseDouble(incomeField.getText())) { // om antalet är mer än hur mycket användaren har
                             label.setText("You don't have that much money");
@@ -50,14 +49,23 @@ public class extractMoney extends JPanel{
 
                             MAIN.changePanel(MAIN.login); // tar tillbaka till menyn
                             afterLogin.incomeField.setText(Double.toString(afterLogin.income)); // ändrar totala inkomst  till det nya inkomst
-
-                            loop = false;
                         }
                     }
 
                     else { // om användaren försöker istället lägga till pengar
                         label.setText("You're supposed to extract money...");
                     }
+                }
+
+                catch(Exception f){
+                    label.setText("What amount do you wish to extract from your account:");
+                    incomeField.setText(Double.toString(afterLogin.income));
+
+                    incomeField.addMouseListener(new MouseAdapter() { // tömmer ruta
+                        public void mouseClicked(MouseEvent e) {
+                            incomeField.setText("");
+                        }
+                    });
                 }
             }
         });
